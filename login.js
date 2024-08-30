16ae7b6092e2245f91228072662e0d28c5d2fd85
saksham30404 <151768784+saksham30404@users.noreply.github.com>
2024-08-30 23:52:06 +0530

    login.js

--- login.js
+++ login.js
@@ -1,24 +1,18 @@
-// Get the form element
-const form = document.getElementById('loginForm');
 
-// Add an event listener to the form's submit event
+const form = document.getElementById('loginForm');
 form.addEventListener('submit', (e) => {
-  // Prevent the default form submission behavior
   e.preventDefault();
 
   // Get the username and password input values
   const username = document.getElementById('username').value;
   const password = document.getElementById('password').value;
-
-  // Your login logic here...
-  // For demonstration purposes, I'll assume a successful login
+  // For demonstration purposes assuming a successful login...
   const isLoggedIn = true;
 
   if (isLoggedIn) {
-    // Redirect to the text file
+    // Redirecting
     window.location.href = 'text.html';
   } else {
-    // Handle login failure
     console.log('Login failed');
   }
 });
